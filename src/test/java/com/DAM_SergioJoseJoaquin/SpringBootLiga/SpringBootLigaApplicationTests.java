87183684e95be6ccc3bbafaa293bc55555705165
Sergio <sergiomarincorrales@gmail.com>
2024-02-06 10:05:37 +0100

    Creado el proyecto con SpringBoot|Dependencias: web, jpa y mysql driver. También creado los diferentes paquetes

---
+++ src/test/java/com/DAM_SergioJoseJoaquin/SpringBootLiga/SpringBootLigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAM_SergioJoseJoaquin.SpringBootLiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
